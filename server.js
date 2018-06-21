13aacb154525a92147c55f8178ac9d304c6adaf6
Kea <keabutler1@gmail.com>
2018-06-21 16:37:40 -0700

    Updated Server

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ app = express(),
 logger = require('morgan'),
 mongoose = require(mongoose),
 PORT = 3000,
-dogRouter = require('./routers/dogRouter')
+
 
 // connect to db
 mongoose.connect('mongodb:/localhost/breeder', (err) => {
@@ -16,7 +16,7 @@ mongoose.connect('mongodb:/localhost/breeder', (err) => {
 app.user(logger('dev'));
 app.use(express.json());
 
-app.use('/api/dogs', dogRouter);
+
 
 
 
